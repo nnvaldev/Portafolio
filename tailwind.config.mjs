25f8770362f815b43fc3bec1753bc9c483fbacf7
nnvaldev <nnvaldev@gmail.com>
2025-03-19 20:38:37 -0400

    primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,vue,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
